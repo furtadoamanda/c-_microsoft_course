4de470074e2421750106ae9aa101cd89720eee4d
Amanda Furtado <furtadodesouzaamanda@gmail.com>
2024-01-23 19:22:23 -0300

    Continuação Módulo 5

--- CSharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CSharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68e316159118df659dbd57e1edca5339c0a938fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebc2d0c1029e744c0d01a7244299e87b9976ef67")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Parte 3 - Adicionar lógica a aplicativos de console em C#/Módulo 5: Adicionar uma lógica de ciclo ao código através das instruções do-while e while em C#/Desafio 1 - Módulo 5/Desafio5.1.cs
+++ Parte 3 - Adicionar lógica a aplicativos de console em C#/Módulo 5: Adicionar uma lógica de ciclo ao código através das instruções do-while e while em C#/Desafio 1 - Módulo 5/Desafio5.1.cs
@@ -17,3 +17,25 @@ do {
 // Erro: não para a execução, está executando tudo sem parar e sem critério
 // Talvez o ataque tenha que ser uma variável diferente para hero e monster
 // As chaves após o while podem ser um problema
+
+// Solução Microsoft:
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
+
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
